1b49d61b843fc8345bef7fbbc010fb31e1bc02df
karlaQuintanaR <s22120215@alumnos.itsur.edu.mx>
2026-02-23 22:43:45 -0600

    intento

--- app/src/main/java/com/example/sicenet/ui/screen/UnidadesScreen.kt
+++ app/src/main/java/com/example/sicenet/ui/screen/UnidadesScreen.kt
@@ -33,6 +33,7 @@ fun UnidadesScreen(vm: SicenetViewModel, onOpenMenu: () -> Unit) {
     // Paleta de colores Café
 
 
+
     val cafeProfundo = Color(0xFF3E2723)
     val cafeMedio = Color(0xFF5D4037)
     val cremaFondo = Color(0xFFF5F5F5)
